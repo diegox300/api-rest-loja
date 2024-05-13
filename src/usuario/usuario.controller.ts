5e1bc2126180e21ac7eb1046a4756500352bb7f5
Diego Gervasio <diegox300@gmail.com>
2024-05-13 17:45:35 -0300

    Finalizando CRUD, adicionando função de atualizar e deletar usuario na API.

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,8 +1,10 @@
-import { Body, Controller, Post, Get } from '@nestjs/common';
+import { Body, Controller, Post, Get, Put, Param, Delete } from '@nestjs/common';
 import { UsuarioRepository } from './usuario.repository';
 import { CriaUsuarioDTO } from './dto/criaUsuario.dto';
 import { UsuarioEntity } from './validacao/usuario.entity';
 import { v4 as uuid } from 'uuid';
+import { ListaUsuarioDTO } from './dto/ListaUsuario.dto';
+import { AtualizaUsuarioDTO } from './dto/AtualizaUsuario.dto';
 
 @Controller('/usuarios')
 export class UsuarioController {
@@ -16,10 +18,41 @@ export class UsuarioController {
     usuarioEntity.nome = dadosDoUsuario.nome;
     usuarioEntity.id = uuid();
     this.usuarioRepository.salvar(usuarioEntity);
-    return { id: usuarioEntity.id, message: 'Usuario criado com sucesso.' };
-  }
+    return { 
+      usuario: new ListaUsuarioDTO(usuarioEntity.id, usuarioEntity.nome), 
+      message: 'Usuario criado com sucesso.' };
+    }
   @Get()
   async listUsuarios() {
-    return this.usuarioRepository.listar();
+    const usuariosSalvos = await this.usuarioRepository.listar();
+    const usuariosLista = usuariosSalvos.map(
+      usuario => new ListaUsuarioDTO(
+        usuario.id,
+        usuario.nome
+      )
+    );
+
+    return usuariosLista;
   }
+
+  @Put('/:id')
+  async atualizaUsuario(@Param('id') id: string,@Body() novosDados: AtualizaUsuarioDTO){
+    const usuarioAtualizado = await this.usuarioRepository.atualiza(id, novosDados);
+
+    return {
+      usuario: usuarioAtualizado,
+      messagem: 'Usuário Atualizado com Sucesso',
+    }
+  };
+
+  @Delete('/:id')
+  async removeUsuario(@Param('id') id: string){
+    const usuarioRemovido = await this.usuarioRepository.remove(id);
+    
+    return {
+      usuario: usuarioRemovido,
+      messagem: 'Usuário removido com sucesso.' 
+    }
+  }
+  
 }
